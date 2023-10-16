944104fd9a9a372345dad9624f3620abeedd7028
diposs <onakunle.dipo@gmail.com>
2023-10-16 09:54:41 +0100

    Responsive Layout  (#41)
    * Update Layout.tsx
    
    * Update Layout.tsx
    
    * Update PersonalSign.tsx

--- src/features/Layout.tsx
+++ src/features/Layout.tsx
@@ -12,7 +12,7 @@ export interface LayoutProps {
 export function Layout({ title, children }: LayoutProps) {
   return (
     <Box width='100%' height='100%' p={8}>
-      <Stack spacing={6}>
+      <Stack spacing={6} display='block' h='full'>
         <Heading color='bw.800'>{title}</Heading>
         <Box height='100%'>
           {children}
